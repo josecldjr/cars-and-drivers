21e500223b8ef80a8e79be7eba47e3f84471e1d0
josecldjr <josecarvalholdjr@gmail.com>
2021-02-06 22:03:45 +0000

    Enabling cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { applicationPort } from './config/env-vars';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     whitelist: true,
   }))
+
+  app.use(cors())
+
   await app.listen(applicationPort);
 }
 bootstrap();
